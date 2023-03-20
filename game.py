6342d8b8651f660a986e436b8f17cfb96adc1d30
SebastianLenart <stentbike7@gmail.com>
2023-03-20 21:27:58 +0100

    v2

--- game.py
+++ game.py
@@ -1,17 +1,56 @@
 from room import Room
 
 class Game:
-    def __init__(self):
-        self.rooms = []
-        self.add_room()
+    MENU = """1.) Nazwa pokoju
+2.) Opis sytuacji
+3.) Opis pokoju
+4.) Lista dostepnych narzedzi
+5.) Opis uzycia narzędzi
+6.) Uzyj narzedzia
+7.) Wyswietl poziom gry
+8.) Wyjscie z gry
+Wybierz opcje: """
 
-    def add_room(self):
-        self.rooms.append(Room(1))
+    def __init__(self):
+        self.level = 1
+        self.rooms = []
+        self.add_room(1)
+        self.options = {
+            "1": self.title,
+            "2": self.described_sytuation,
+            "3": self.described_room,
+            "4": self.tools,
+            "5": self.title,
+        }
+        self.add_room(self.level)
+
+    def add_room(self, number_room):
+        self.rooms.append(Room(number_room))
         self.rooms[0].load_room()
-        self.rooms[0].get_items_and_answer()
+
+
+    def title(self):
+        print("Nazwa pokoju: ", self.rooms[0].get_title())
+
+    def described_sytuation(self):
+        print(self.rooms[0].get_described_sytuation())
+
+    def described_room(self):
+        print(self.rooms[0].get_described_room())
+
+    def tools(self):
+        print(*list(self.rooms[0].get_items()))
+
+    def start(self):
+        while (selection := input(self.MENU)) != "8":
+            try:
+                self.options[selection]()
+            except KeyError:
+                print("Niepoprawna opcja, powtorz.")
 
 
 
 
 if __name__ == "__main__":
     game = Game()
+    game.start()
